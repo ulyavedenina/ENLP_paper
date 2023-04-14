84baea5ca7d600fba37ab4495ae44886ad798a8a
ulyavedenina <vedeninaulya@gmail.com>
2023-04-15 01:18:15 +0200

    add evaluation

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ lexicon_path = 'data/Teko/data/eme_traindev.txt'
 # the path to the Hex-encoded train+dev data set
 encoded_lexicon_path = 'data/Teko/data/lexicon.txt'
 # the path to SFG used (9 in total)
-grammar_number = 19
+grammar_number = 0
 grammar_path = f'data/Teko/grammar/standard/grammar{grammar_number}.txt'
 # the path to the SFG used + appended Hex-encoded chars as terminals
 final_grammar_path = f'data/Teko/grammar/standard/final_grammar{grammar_number}.txt'
@@ -26,50 +26,50 @@ write_encoded_words(encoded_words, encoded_lexicon_path)
 grammar = read_grammar(grammar_path)
 appended_grammar = add_chars_to_grammar(grammar, hex_chars)
 write_grammar(appended_grammar, final_grammar_path)
-#
-# # ---training---
-# # After that, the Pitman-Yor Adaptor-Grammar Sampler (PYAGS) was run (on AWS Cloud)
-# # The results of the runs are located in data/Teko/pycfg/standard/output
-#
-# # ---segmentation---
-# # If a word is seen in the training data, its segmentation
-# # is read from the segmentation output of the learning process.
-# # If a word is not seen in the training data, it's analyzed
-# # by finding the split that gives the highest MLE probability
-# # across its morphemes, along with the selection of compatible
-# # prefixes and suffixes. The information of the morphemes and their
-# # MLE probabilities and compatibility are driven from the segmentation
-# # output of the learning process. # This method is applicable only when
-# # the prefixes, stems and suffixes are represented by three different
-# # nonterminals.
-#
-# # Create a segmentation model given the PYAGS segmentation output.
-# # The step requires specifying which nonterminals to split on.
-# # In addition to generating the segmentation model, the step generates
-# # a human-readable segmentation output that can be directly used as the
-# # prediction input for the evaluation scripts used in the Morpho-Challenge
-# # shared task.
-# # The parameter is optional. If used, it should be the ISO-639-1 language
-# # code, and it only affects Turkish (for its special lowercasing and uppercasing).
-#
-# # path to the produced output during the PYAGS training
-# pyags_output_grammar_path = '/Users/ulyavedenina/Documents/MorphAGram/data/Teko/pycfg/standard/output/grammar-traindev.cfg1-s.txt'
-#
-# segmentation_model = parse_segmentation_output(pyags_output_grammar_path,
-#                                                'Prefix', 'Stem', 'Suffix', '/Users/ulyavedenina/Documents/MorphAGram/data/Teko/pycfg/standard/output/out.txt',
-#                                                 None, 1)
-#
-# # segment validation and test sets. Validation test -- was seen during the training (train+dev); test set -- unseen
-# segment_file('data/Teko/data/eme_dev_unseg.txt', 'data/Teko/data/eme_dev_res.txt', segmentation_model, ' ', ' ', False, None , 1)
-# segment_file('data/Teko/data/eme_test_unseg.txt', 'data/Teko/data/eme_test_res.txt', segmentation_model, ' ', ' ', False, None , 1)
-#
-# # data set and output statistics
-# gold_path = 'data/Teko/data/eme_dev.txt'
-# output_path = 'data/Teko/pycfg/standard/output/out.txt'
-# gold_info, morph_info = analyze_gold(gold_path)
-# morph_out_info = analyze_output(output_path, gold_path)
-#
-# # ---evaluation---
-# # evaluation for BPR and Emma-2 was performed with morphoeval library using the following script:
-# # morphoeval [--metric] gold_values_file predicted_value_file output_file
-# # the results are stored in /Users/ulyavedenina/Documents/MorphAGram/data/Teko/data
+
+# ---training---
+# After that, the Pitman-Yor Adaptor-Grammar Sampler (PYAGS) was run (on AWS Cloud)
+# The results of the runs are located in data/Teko/pycfg/standard/output
+
+# ---segmentation---
+# If a word is seen in the training data, its segmentation
+# is read from the segmentation output of the learning process.
+# If a word is not seen in the training data, it's analyzed
+# by finding the split that gives the highest MLE probability
+# across its morphemes, along with the selection of compatible
+# prefixes and suffixes. The information of the morphemes and their
+# MLE probabilities and compatibility are driven from the segmentation
+# output of the learning process. # This method is applicable only when
+# the prefixes, stems and suffixes are represented by three different
+# nonterminals.
+
+# Create a segmentation model given the PYAGS segmentation output.
+# The step requires specifying which nonterminals to split on.
+# In addition to generating the segmentation model, the step generates
+# a human-readable segmentation output that can be directly used as the
+# prediction input for the evaluation scripts used in the Morpho-Challenge
+# shared task.
+# The parameter is optional. If used, it should be the ISO-639-1 language
+# code, and it only affects Turkish (for its special lowercasing and uppercasing).
+
+# path to the produced output during the PYAGS training
+pyags_output_grammar_path = '/Users/ulyavedenina/Documents/MorphAGram/data/Teko/pycfg/standard/output/output-traindev.cfg19.txt'
+
+segmentation_model = parse_segmentation_output(pyags_output_grammar_path,
+                                               'PrefixMorphs', 'Stem', 'SuffixMorphs', '/Users/ulyavedenina/Documents/MorphAGram/data/Teko/pycfg/standard/output/segmentation-traindev.cfg19.txt',
+                                                None, 1)
+
+# segment validation and test sets. Validation test -- was seen during the training (train+dev); test set -- unseen
+segment_file('data/Teko/data/eme_dev_unseg.txt', 'data/Teko/data/eme_dev_res-cfg19.txt', segmentation_model, ' ', ' ', False, None , 1)
+segment_file('data/Teko/data/eme_test_unseg.txt', 'data/Teko/data/eme_test_res-cfg19.txt', segmentation_model, ' ', ' ', False, None , 1)
+
+# data set and output statistics
+gold_path = 'data/Teko/data/eme_dev_true.txt'
+output_path = 'data/Teko/pycfg/standard/output/out.txt'
+gold_info, morph_info = analyze_gold(gold_path)
+morph_out_info = analyze_output(output_path, gold_path)
+
+# ---evaluation---
+# evaluation for BPR and Emma-2 was performed with morphoeval library using the following script:
+# morphoeval [--metric] gold_values_file predicted_value_file output_file
+# the results are stored in /Users/ulyavedenina/Documents/MorphAGram/data/Teko/data
